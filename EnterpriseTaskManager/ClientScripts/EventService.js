28101144b653cfb88964aa9519a3131dcd202c44
PayalMantri10 <Payal.mantri@accoliteindia.com>
2017-08-01 00:18:35 +0530

    Update the Ui and associated methods

---
+++ EnterpriseTaskManager/ClientScripts/EventService.js
@@ -0,0 +1,21 @@
+﻿app.service("EventService", function ($http) {
+
+    this.getEventTransactionlist = function () {
+        var response = $http.get('/Home/GetEventTransactionList' );
+       
+        return response;
+    };
+
+    this.getEventTypeDetails = function (EventType) {
+        var response = $http.get('/Home/GetEventTypeDetails?EventType='+EventType );
+        return response;
+};
+
+
+
+    this.postInfo = function (insurance) {
+        var req = $http.post('/api/Insurance', insurance);
+        return req;
+    };
+
+});
